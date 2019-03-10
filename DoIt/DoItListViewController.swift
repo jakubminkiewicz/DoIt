1f62c70c92c57c30ab3f566fda401296f92799ca
Jakub Minkiewicz <32709219+jakubminkiewicz@users.noreply.github.com>
2019-03-10 17:07:01 +0000

    Adding new items to list complete.

--- DoIt/DoItListViewController.swift
+++ DoIt/DoItListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class DoItListViewController: UITableViewController {
     
-    let itemArray = ["Find egg","Eat egg","Worship egg"]
+    var itemArray = ["Find egg","Eat egg","Worship egg"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,7 +45,32 @@ class DoItListViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //MARK - Add New Items
 
-
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //What will happen once user clicks the Add Item button
+            self.itemArray.append(textField.text ?? "")
+            
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
 }
-
